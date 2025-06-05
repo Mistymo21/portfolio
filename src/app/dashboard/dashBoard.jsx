79f557ed81ee24dfa9260bac51894e7fead6b448
Mistymo21 <yusufmm90999@gmail.com>
2025-06-05 21:29:13 +0100

    DS

--- src/app/dashboard/dashBoard.jsx
+++ src/app/dashboard/dashBoard.jsx
@@ -7,12 +7,7 @@ const DashBoard = () => {
         <h1 className={styles.title}>Let's Work Together!</h1>
         <p className={styles.name}>Hi, I'm Yusuf,</p>
         <p className={styles.text}>
-          A Frontend Developer focused on building clean, responsive, and
-          user-friendly interfaces. I specialize in Next.js, React, and modern
-          CSS techniques, delivering polished web experiences that prioritize
-          performance, accessibility, and design accuracy. I'm passionate about
-          creating scalable components and smooth user interactions that bring
-          interfaces to life.
+         I'm a Full Stack Developer with a strong focus on building clean, responsive, and user-friendly web applications. I specialize in Next.js, React, and modern CSS on the frontend, and work with Node.js, MongoDB, and RESTful APIs on the backend. I’m dedicated to delivering fast, accessible, and scalable solutions that align with both user needs and business goals. I take pride in writing clean code, designing intuitive interfaces, and developing reliable backend systems that work seamlessly together.
         </p>
       </div>
     </div>
